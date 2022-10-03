f5485290775445c101e3149531b075371b53f913
jehan023 <jehanhadjisaid.023@gmail.com>
2022-10-03 15:30:55 +0800

    Edit Titles

--- src/components/company-profile.js
+++ src/components/company-profile.js
@@ -138,7 +138,7 @@ function CompanyProfile() {
                     <img src={CEO} alt='ceo' className='ceo-img' />
                     <div className='ceo-details'>
                         <p className='ceo-name fw-bold'>Atty. Marlon Valderama</p>
-                        <p className='ceo-position'>CEO and Founder</p>
+                        <p className='ceo-position'>President and CEO</p>
                         <p className='ceo-position'>LexMeet, Inc.</p>
                     </div>
                 </div>
